f669fa6b20fbbf36a9266cfb95da6421cb99f108
vasandris01 <undris01@gmail.com>
2023-02-22 12:57:50 +0100

    Initial commit

---
+++ src/main/java/com/tradinggame/kalmar/KalmarApplication.java
@@ -0,0 +1,13 @@
+package com.tradinggame.kalmar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalmarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalmarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tradinggame/kalmar/KalmarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradinggame.kalmar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalmarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
