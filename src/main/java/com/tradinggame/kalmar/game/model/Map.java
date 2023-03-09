99de35ee2cf995d08cdf06d341f858e385e6f79e
vasandris01 <undris01@gmail.com>
2023-03-09 15:23:41 +0100

    model for maps

---
+++ src/main/java/com/tradinggame/kalmar/game/model/Map.java
@@ -0,0 +1,7 @@
+package com.tradinggame.kalmar.game.model;
+
+public class Map {
+    private Tile[][] tiles;
+    private int[][] mapAsMatrix = "${}";
+
+}
---
+++ src/main/java/com/tradinggame/kalmar/game/model/Tile.java
@@ -0,0 +1,8 @@
+package com.tradinggame.kalmar.game.model;
+
+import java.awt.*;
+
+public class Tile {
+    private Image image;
+    private Type type;
+}
