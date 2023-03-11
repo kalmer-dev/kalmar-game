ed1d1a942de5cf293a91714c73c4f6d5f7c38dec
vasandris01 <undris01@gmail.com>
2023-03-11 11:09:59 +0100

    test data on map

--- src/main/java/com/tradinggame/kalmar/game/model/Tile.java
+++ src/main/java/com/tradinggame/kalmar/game/model/Tile.java
@@ -1,14 +1,32 @@
 package com.tradinggame.kalmar.game.model;
 
-import java.awt.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Tile {
-    private Image image;
+    private BufferedImage image;
     private Type type;
 
-    public Tile(int identifier){
-        switch (identifier){
-            case 1 -> { this.type = Type.GRASS;}
+    public Tile(int identifier) {
+        try {
+            switch (identifier) {
+                case 1 -> {
+                    this.type = Type.GRASS;
+                    File file = new File("src/main/resources/images/map/OneColorBackgrounds/green.png");
+                    this.image = ImageIO.read(file);
+
+                }
+                case 2 ->{
+                    this.type = Type.TRADING_POST;
+                    File file = new File("src/main/resources/images/map/OneColorBackgrounds/brown.png");
+                    this.image = ImageIO.read(file);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
