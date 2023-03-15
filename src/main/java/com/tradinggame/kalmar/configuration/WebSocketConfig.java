7d8a6b58024cea31c688ffed17220b635abae1a8
vasandris01 <undris01@gmail.com>
2023-03-15 17:54:21 +0100

    commit wrong version

--- src/main/java/com/tradinggame/kalmar/configuration/WebSocketConfig.java
+++ src/main/java/com/tradinggame/kalmar/configuration/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(lobbyHandler, "/lobby").setAllowedOrigins("*");
+        registry.addHandler(lobbyHandler, "/game_lobby").setAllowedOrigins("*");
     }
 }
 
--- src/main/java/com/tradinggame/kalmar/controller/WebSocketController.java
+++ src/main/java/com/tradinggame/kalmar/controller/WebSocketController.java
@@ -1,6 +1,5 @@
 package com.tradinggame.kalmar.controller;
 
-
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,9 +7,15 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WebSocketController {
 
-    @MessageMapping("/lobby")
-    @SendTo("/lobby")
-    public String handleLobbyMessage(String message) {
-        return "Új felhasználó csatlakozott: " + message;
+    @MessageMapping("/join")
+    @SendTo("/topic/game_lobby")
+    public String join(String username) {
+        return username + " joined the lobby";
+    }
+
+    @MessageMapping("/send")
+    @SendTo("/topic/game_lobby")
+    public String sendMessage(String message) {
+        return message;
     }
 }
