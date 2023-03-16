fe6f4e723a62e98515458c7bb3b4786f5934b406
btnd7 <gaal.botond.16@gmail.com>
2023-03-16 13:39:39 +0100

    updated Map

--- src/main/java/com/tradinggame/kalmar/controller/MenuController.java
+++ src/main/java/com/tradinggame/kalmar/controller/MenuController.java
@@ -4,9 +4,7 @@ import com.tradinggame.kalmar.game.model.Game;
 import com.tradinggame.kalmar.game.model.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
