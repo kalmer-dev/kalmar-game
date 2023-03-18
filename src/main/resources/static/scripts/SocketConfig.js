7a282c22fe04ee2c875e68afdb7b5942e0ab0f3a
vasandris01 <undris01@gmail.com>
2023-03-18 09:55:32 +0100

    last wrong version

--- src/main/resources/static/scripts/SocketConfig.js
+++ src/main/resources/static/scripts/SocketConfig.js
@@ -3,26 +3,31 @@ var stompClient = null;
 function connect(gameid) {
     var socket = new SockJS('/lobby/' + gameid);
     stompClient = Stomp.over(socket);
-
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/lobby/' + gameid, function (response) {
+            console.log("Received message: " + response.body);
+        });
+    });
 }
-
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
-}
-
-function disconnect() {
-    if (stompClient !== null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
-}
+//
+// function setConnected(connected) {
+//     $("#connect").prop("disabled", connected);
+//     $("#disconnect").prop("disabled", !connected);
+//     if (connected) {
+//         $("#conversation").show();
+//     }
+//     else {
+//         $("#conversation").hide();
+//     }
+//     $("#greetings").html("");
+// }
+//
+// function disconnect() {
+//     if (stompClient !== null) {
+//         stompClient.disconnect();
+//     }
+//     setConnected(false);
+//     console.log("Disconnected");
+// }
 
