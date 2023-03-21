20b77164f456e1f48111b5fc831397d68e79eb90
Mikey <mikey.mazza1999@gmail.com>
2023-03-21 17:49:27 +0100

    Spring_Security_reach

--- src/main/java/com/tradinggame/kalmar/model/UserService.java
+++ src/main/java/com/tradinggame/kalmar/model/UserService.java
@@ -34,9 +34,9 @@ public class UserService implements UserDetailsService {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
-                .requestMatchers( "/", "/testing", "/registration","/Map","/lobby","/index","/home","/connect")
+                .requestMatchers( "/", "/testing", "/registration" )
                 .permitAll().anyRequest().authenticated().and().formLogin()
-                .defaultSuccessUrl("/testing").defaultSuccessUrl("/Map").defaultSuccessUrl("/lobby").defaultSuccessUrl("/index").defaultSuccessUrl("/connect")
+                .defaultSuccessUrl("/home")
                 .permitAll()
                 .and()
                 .logout()
