05da86f4581d64fc2b04566c43e1f1e3beeb42c7
Monyii <b.monyi17@gmail.com>
2023-03-29 10:24:08 +0200

    Map, Matrix Reader

--- src/main/java/com/tradinggame/kalmar/game/model/Map.java
+++ src/main/java/com/tradinggame/kalmar/game/model/Map.java
@@ -1,32 +1,29 @@
 package com.tradinggame.kalmar.game.model;
 
-
+import com.google.gson.Gson;
 import lombok.Getter;
+
+import java.io.File;
 
 public class Map {
 
-    private final int[][] mapAsMatrix = new int[][]{
-            {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
-            {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0}};
+<<<<<<< HEAD
+    private final int[][] mapAsMatrix =
+    private final String[][] mapAsMatrix;
+
+>>>>>>> fb9e952 (Megjelenítés Version 2. (CSS és HTML hibajavítás))
     @Getter
     private Tile[][] tiles;
 
     public Map() {
+        File file=new File("src/main/resources/static/Map/1.json");
+        String string=file.toString();
+        Gson gson = new Gson();
+        mapAsMatrix = gson.fromJson(string, String[][].class);
         tiles = new Tile[mapAsMatrix.length][mapAsMatrix[0].length];
         for (int rows = 0; rows < mapAsMatrix.length; rows++) {
             for (int column = 0; column < mapAsMatrix[rows].length; column++) {
-                tiles[rows][column] = new Tile(mapAsMatrix[rows][column]);
+                tiles[rows][column] = new Tile(Integer.parseInt(mapAsMatrix[rows][column]));
 
             }
         }
