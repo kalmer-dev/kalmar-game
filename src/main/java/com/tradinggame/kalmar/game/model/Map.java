7984715afa189139b86c14b93348d873b11d8da4
Monyii <b.monyi17@gmail.com>
2023-03-29 13:26:52 +0200

    50x50 matrix pálya json-ből

--- src/main/java/com/tradinggame/kalmar/game/model/Map.java
+++ src/main/java/com/tradinggame/kalmar/game/model/Map.java
@@ -3,29 +3,37 @@ package com.tradinggame.kalmar.game.model;
 import com.google.gson.Gson;
 import lombok.Getter;
 
-import java.io.File;
+import java.io.*;
 
 public class Map {
 
-<<<<<<< HEAD
-    private final int[][] mapAsMatrix =
+
+    //    private final int[][] mapAsMatrix =
     private final String[][] mapAsMatrix;
 
->>>>>>> fb9e952 (Megjelenítés Version 2. (CSS és HTML hibajavítás))
+
     @Getter
     private Tile[][] tiles;
 
     public Map() {
-        File file=new File("src/main/resources/static/Map/1.json");
-        String string=file.toString();
-        Gson gson = new Gson();
-        mapAsMatrix = gson.fromJson(string, String[][].class);
-        tiles = new Tile[mapAsMatrix.length][mapAsMatrix[0].length];
-        for (int rows = 0; rows < mapAsMatrix.length; rows++) {
-            for (int column = 0; column < mapAsMatrix[rows].length; column++) {
-                tiles[rows][column] = new Tile(Integer.parseInt(mapAsMatrix[rows][column]));
+        try {
 
+
+            File file = new File("src/main/resources/static/Map/1.json");
+            Gson gson = new Gson();
+            Reader reader = new FileReader(file);
+            mapAsMatrix = gson.fromJson(reader, String[][].class);
+            tiles = new Tile[mapAsMatrix.length][mapAsMatrix[0].length];
+            for (int rows = 0; rows < mapAsMatrix.length; rows++) {
+                for (int column = 0; column < mapAsMatrix[rows].length; column++) {
+                    tiles[rows][column] = new Tile(Integer.parseInt(mapAsMatrix[rows][column]));
+                }
             }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
