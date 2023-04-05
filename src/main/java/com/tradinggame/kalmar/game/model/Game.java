18d9eb4c0090a09e2cae347a0c72d773ba02eec3
vasandris01 <undris01@gmail.com>
2023-04-05 11:12:05 +0200

    game end in 60 sec

--- src/main/java/com/tradinggame/kalmar/game/model/Game.java
+++ src/main/java/com/tradinggame/kalmar/game/model/Game.java
@@ -58,7 +58,7 @@ public class Game implements Runnable {
     @Override
     public void run() {
         try {
-            thread.sleep(10000);
+            thread.sleep(60000);
         } catch (InterruptedException e) {
             System.out.println("bent");
         }
